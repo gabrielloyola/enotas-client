e3977c5fe62fc76bd56d17c377c5cce50b26270d
Thiago Dias <tdtadeu@gmail.com>
2018-01-31 18:54:33 -0200

    Added missing attributes for ICMS

--- lib/enotas/model/impostos/icms.rb
+++ lib/enotas/model/impostos/icms.rb
@@ -6,6 +6,15 @@ module Enotas
 
         attribute :situacaoTributaria, String
         attribute :origem, Integer
+        attribute :aliquota, Integer
+        attribute :baseCalculo, Integer
+        attribute :modalidadeBaseCalculo, Integer
+        attribute :percentualReducaoBaseCalculo, Integer
+        attribute :baseCalculoST, Integer
+        attribute :aliquotaST, Integer
+        attribute :modalidadeBaseCalculoST, Integer
+        attribute :percentualReducaoBaseCalculoST, Integer
+        attribute :percentualMargemValorAdicionadoST, Integer
       end
     end
   end
