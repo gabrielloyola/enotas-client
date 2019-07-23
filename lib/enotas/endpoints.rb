3d4bb9c82b36dbe7e1b5975cd00e041956b7f1ee
gabrielloyola <gabrielfrml@gmail.com>
2019-07-23 17:17:25 -0300

    Add business list endpoint

--- lib/enotas/endpoints.rb
+++ lib/enotas/endpoints.rb
@@ -1,5 +1,13 @@
 module Enotas
   module Endpoints
+    def check_company(company_id)
+      get "empresas/#{company_id}"
+    end
+
+    def get_company_list(page_number: 0, page_size: 5)
+      get "empresas?pageNumber=#{page_number}&pageSize=#{page_size}"
+    end
+
     def create_nfe(company_id, body = {})
       post "empresas/#{company_id}/nf-e", body
     end
@@ -14,43 +22,43 @@ module Enotas
 
     # TODO redo this whole list
     def nfse_list(company_id, body = {})
-      get("empresas/#{company_id}/nfes", body)
+      get "empresas/#{company_id}/nfes", body
     end
 
     def nfse_create(company_id, body)
-      post("empresas/#{company_id}/nfes", body)
+      post "empresas/#{company_id}/nfes", body
     end
 
     def nfse_delete(company_id, nfe_id)
-      delete("empresas/#{company_id}/nfes/#{nfe_id}")
+      delete "empresas/#{company_id}/nfes/#{nfe_id}"
     end
 
     def nfse_get(company_id, id)
-      get("empresas/#{company_id}/nfes/#{id}")
+      get "empresas/#{company_id}/nfes/#{id}"
     end
 
     def nfse_get_by_external_id(company_id, external_id)
-      get("empresas/#{company_id}/nfes/#{external_id}")
+      get "empresas/#{company_id}/nfes/#{external_id}"
     end
 
     def nfse_delete_by_external_id(company_id, external_id)
-      delete("empresas/#{company_id}/nfes/#{external_id}")
+      delete "empresas/#{company_id}/nfes/#{external_id}"
     end
 
     def nfse_get_pdf(company_id, id)
-      get("empresas/#{company_id}/nfes/#{id}")
+      get "empresas/#{company_id}/nfes/#{id}"
     end
 
     def nfse_get_pdf_by_external_id(company_id, external_id)
-      get("empresas/#{company_id}/nfes/#{external_id}/pdf")
+      get "empresas/#{company_id}/nfes/#{external_id}/pdf"
     end
 
     def nfse_get_xml(company_id, id)
-      get("empresas/#{company_id}/nfes/#{id}/xml")
+      get "empresas/#{company_id}/nfes/#{id}/xml"
     end
 
     def nfse_get_xml_by_external_id(company_id, external_id)
-      get("empresas/#{company_id}/nfes/#{external_id}/xml")
+      get "empresas/#{company_id}/nfes/#{external_id}/xml"
     end
   end
 end
